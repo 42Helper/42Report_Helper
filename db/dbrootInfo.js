dc071c8a80be66390b3f874783277d687093f406
gynhyunkim <gyeonghyun.kimm@gmail.com>
2021-03-29 14:14:00 +0900

    fix: env_var

--- db/dbrootInfo.js
+++ db/dbrootInfo.js
@@ -1,7 +1,7 @@
 var root = {
     host: process.env.DB_ROOT,
     user: process.env.DB_USER,
-    password: DB_PASSWORD,
+    password: process.env.DB_PASSWORD,
     database: "rebot",
     multipleStatements: true,
 };
