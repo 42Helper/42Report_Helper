d356b9de36ab1a21ebbd3335393a0a52e6107ebc
HyeJu99 <sinhye102@gmail.com>
2021-04-07 15:46:00 +0900

    테이블 자동초기화 테스팅

--- main.js
+++ main.js
@@ -295,5 +295,7 @@ const isresetWeek = require("./Report/resetcount.js");
     schedule.scheduleJob(`00 17 * * 7`, function () {
         sendMsg.sundayMsg();
     });
-    schedule.scheduleJob(`43 15 7 4 *`, isresetWeek());
+    schedule.scheduleJob(`47 15 7 4 *`, function () {
+        isresetWeek();
+    });
 })();
