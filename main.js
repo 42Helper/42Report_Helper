edc47893e83cbb82d843c8facde47a4a95636780
HyeJu99 <sinhye102@gmail.com>
2021-04-07 15:54:08 +0900

    add: 테이블 자동 초기화 기능 week1 월요일 자정마다 user-week1~4 컬럼 및 report-전체 초기화

--- main.js
+++ main.js
@@ -295,5 +295,5 @@ const isresetWeek = require("./Report/resetcount.js");
     schedule.scheduleJob(`00 17 * * 7`, function () {
         sendMsg.sundayMsg();
     });
-    schedule.scheduleJob(`52 15 7 4 *`, isresetWeek);
+    schedule.scheduleJob(`00 00 * * 0`, isresetWeek);
 })();
