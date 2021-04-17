6be810b6d4d03189fbe8ec1408240b2e029e3e71
hysimok <jeonghy95@gmail.com>
2021-04-18 02:54:22 +0900

    FEAT: prevent button click weeks later

--- Utils/getperiod.js
+++ Utils/getperiod.js
@@ -1,11 +1,11 @@
 const db = require('../db/dbconnection');
 
-let getPeriod = function () {
+let getPeriod = function (date) {
     return new Promise(function(resolve, reject) {
         try{
             db.query(`SELECT week
             FROM period
-            WHERE now() >= start_of_week AND now() <= end_of_week;`,
+            WHERE "${date}" >= start_of_week AND "${date}" <= end_of_week;`,
             function(error, results){
                 resolve(results[0].week);
             });
